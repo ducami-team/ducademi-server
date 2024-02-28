64cc2feebed8064d7c3d0d25a92b0697c9b79e64
재명 <ljm9894@dgsw.hs.kr>
2024-02-29 00:58:56 +0900

    teset

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -4,22 +4,22 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
 @Injectable()
 export class MysqlConnectService implements TypeOrmOptionsFactory {
-    constructor(private readonly configService : ConfigService){}
-    createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-        Logger.log(this.configService.get<string>('DATABASE_HOST'));
-        return { 
-            type : 'mysql',
-            host : this.configService.get<string>('DATABASE_HOST'),
-            port : this.configService.get<number>('DATABASE_PORT'),
-            username : this.configService.get<string>('DATABASE_USERNAME'),
-            password : this.configService.get<string>('DATABASE_PASSWORD'),
-            database : this.configService.get<string>('DATABASE_DATABASE'),
-            entities : [__dirname + '/../**/*.entity{/.ts,.js}'],
-            // timezone : 'Asia/Seoul',
-            synchronize : true,
-            autoLoadEntities : true,
-            logging : false,
-        }
-    }
-    
+  constructor(private readonly configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    Logger.log(this.configService.get<string>('DATABASE_HOST'));
+    return {
+      type: 'mysql',
+      // host : this.configService.get<string>('DATABASE_HOST'),
+      host: 'ducademi.cbh607ibpkv8.ap-northeast-2.rds.amazonaws.com',
+      port: this.configService.get<number>('DATABASE_PORT'),
+      username: this.configService.get<string>('DATABASE_USERNAME'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+      database: this.configService.get<string>('DATABASE_DATABASE'),
+      entities: [__dirname + '/../**/*.entity{/.ts,.js}'],
+      // timezone : 'Asia/Seoul',
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: false,
+    };
+  }
 }
